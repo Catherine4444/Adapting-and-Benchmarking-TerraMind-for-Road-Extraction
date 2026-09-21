e38a412046c268493847efb39dfb7dc755a9ed74
Catherine4444 <130481948+Catherine4444@users.noreply.github.com>
2026-09-21 22:47:37 +0200

    .

--- src/finetuning_tm/kaggle_prep/prep_env.bash
+++ src/finetuning_tm/kaggle_prep/prep_env.bash
@@ -5,6 +5,6 @@ cd /kaggle/working
 echo "Installing python dependencies..."
 
 pip install uv
-uv pip install --system -e "InstaRoadPrototype[terra,unet, dlinknet, finetuning_tm]"
+uv pip install --system -e "Adapting-and-Benchmarking-TerraMind-for-Road-Extraction[terra,unet, dlinknet, finetuning_tm]"
 
 echo "Environment ready! "
